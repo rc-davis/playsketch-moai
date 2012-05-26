6dc98e86f76cd60bab6cd885503461d5b2d144b6
Ryder Ziola <ryder@ryderziola.ca>
2012-05-26 08:42:41 +0800

    Create new base model set
    This will replace the current "model" directory

---
+++ src/common-lua/basemodel/basemodel.lua
@@ -0,0 +1,87 @@
+--[[
+	--------------
+	Copyright 2012 Singapore Management University
+	 
+	This Source Code Form is subject to the terms of the
+	Mozilla Public License, v. 2.0. If a copy of the MPL was
+	not distributed with this file, You can obtain one at
+	http://mozilla.org/MPL/2.0/.
+	--------------
+--]]
+
+--[[
+
+	basemodel/basemodel.lua
+	
+	TODO:describe
+
+--]]
+
+
+basemodel = {}
+
+basemodel.motionTypes = {SCALE=1, ROTATE=2, TRANSLATE=3, VISIBILITY=4}
+
+--require "basemodel/timelist"
+--require "basemodel/path"
+--require "basemodel/keyframe"
+
+
+local allDrawables = {}
+local allPaths = {}
+
+function basemodel.allPaths()
+	return allPaths
+end
+
+function basemodel.allDrawables()
+	return allDrawables
+end
+
+function basemodel.addNewDrawable(drawable, time, location)
+
+	-- add to the scene
+	
+	-- create new path
+	
+	-- set path to location
+	
+	-- set visibility
+
+
+end
+
+
+function basemodel.addNewDrawables(drawableList, timeList, locationList)
+
+	assert(#drawableList == #timeList and #drawableList == #locationList, 
+		"addNewDrawables() requires three lists of the same length")
+
+	local addedDrawables = {}
+	for i=1,#drawableList do
+		local newDrawable = basemodel.addNewDrawable(drawablesList[i], timeList[i], locationList[i])
+		table.insert(addedDrawables, newDrawable)
+	end
+	return addedDrawables
+end
+
+
+--[[
+
+
+
+- basemodel.removeDrawable(drawable) -> success
+- basemodel.removeDrawables(drawableList) -> success
+- basemodel.movePathBeforePathAtIndex(path, index) -> new index
+- basemodel.movePathToTop(path) -> new index
+- basemodel.createNewPath(index, drawablesSet) -> path
+- basemodel.drawablesVisibleAtTime(time) -> drawableList
+- basemodel.drawablesForPath(path) -> drawableList
+- basemodel.pathsForDrawable(drawable) -> pathList
+--]]
+
+
+
+
+
+return basemodel
---
+++ src/common-lua/basemodel/timelist.lua
@@ -0,0 +1,168 @@
+--[[
+	--------------
+	Copyright 2012 Singapore Management University
+	 
+	This Source Code Form is subject to the terms of the
+	Mozilla Public License, v. 2.0. If a copy of the MPL was
+	not distributed with this file, You can obtain one at
+	http://mozilla.org/MPL/2.0/.
+	--------------
+--]]
+
+--[[
+
+	basemodel/timelist.lua
+
+	The most basic data structure for holding the animation steps for an object in time.
+	It creates a list of nodes with values associated with a time.
+	When looking up the value at a point in time, it returns the node immediately <= to the requested time
+
+	-- TODO: REPLACE OUR streams LISTS with some btrees with iterator, for faster lookup
+
+--]]
+
+
+basemodel.timelist = {}
+
+local TimeList = {}
+
+function basemodel.timelist.new()
+	local l = {}
+	for i,v in pairs(TimeList) do
+		l[i] = v
+	end
+	l:init()
+	return l
+end
+
+function basemodel.timelist.newFromTable(t)
+	local l = basemodel.timelist.new()
+	l:loadFromTable(t)
+	return l
+end
+
+
+----- TimeList methods -----
+
+function TimeList:init()
+	self.class = "TimeList"
+	self.firstFrame = { time=-1e100, value=nil, nextFrame={ time = 1e100, value=nil }}
+end
+
+
+-- getFrameForTime(time):	Returns the node with the time <= 'time'
+function TimeList:getFrameForTime(time)
+	local frame = self.firstFrame
+	while 	frame ~= nil and
+			frame.nextFrame ~= nil 
+			and frame.nextFrame.time <= time do
+		frame = frame.nextFrame	
+	end
+	return frame
+end
+
+
+-- makeFrameForTime(time):	Creates and returns a new frame at the right place in the linked list
+function TimeList:makeFrameForTime(time)
+
+	local previousFrame = self:getFrameForTime(time)
+	assert(previousFrame ~= nil, "shouldn't be making a frame without a previous frame")
+
+	if previousFrame.time == time then
+		return previousFrame
+	else
+		assert(previousFrame.time < time and previousFrame.nextFrame.time > time, 
+			"we should be inserting a frame to maintain a strict ordering!")
+
+		--make a new frame
+		local newFrame = {	time=time,
+							nextFrame = previousFrame.nextFrame }
+		previousFrame.nextFrame = newFrame
+		return newFrame
+	end
+end
+
+
+-- setValueForTime(time, value): Sets 'value' at 'time', replacing a pre-existing value at the EXACT same time
+function TimeList:setValueForTime(time, value)
+	local frame = self:makeFrameForTime(time)
+	assert(frame ~= nil, "must retrieve a non-nil frame for any given time")		
+	frame.value = value
+end
+
+
+-- getValueForTime(time): returns the value from the frame immediately <= 'time'
+function TimeList:getValueForTime(time)
+	local frame = self:getFrameForTime(time)
+	assert(frame ~= nil, "must retrieve a non-nil frame for any given time")
+	if not frame.value then return frame.nextFrame.value
+	else return frame.value end
+end
+
+
+-- getInterpolatedValueForTime(time): interpolates the value between the frames around 'time' 
+function TimeList:getInterpolatedValueForTime(time)
+	local frame_before = self:getFrameForTime(time)
+	assert(frame_before ~= nil, "must retrieve a non-nil frame for any given time")
+	local frame_after = frame_before.nextFrame
+	
+	if frame_after == nil or frame_after.value == nil then 
+		return frame_before.value 
+	elseif frame_before.value == nil then 
+		return frame_after.value 
+	else
+		local pcnt = (time-frame_before.time)/(frame_after.time - frame_before.time)
+		local interp = nil
+		
+		--interpolate tables and single numbers
+		if type(frame_before.value) == "number" then
+			interp = frame_before.value*(1-pcnt) + frame_after.value*(pcnt)
+		elseif type(frame_before.value) == "table" then
+			interp = {}
+			for k,v in pairs(frame_before.value) do
+				if frame_after.value then
+					interp[k] = v*(1-pcnt) + frame_after.value[k]*(pcnt)
+				end
+			end
+		end
+		return interp
+	end
+end
+
+function TimeList:erase(time, duration)
+	local first = self:getFrameForTime(time)
+	local count = 0
+	while first.nextFrame and first.nextFrame.time < time + duration do
+		first.nextFrame = first.nextFrame.nextFrame
+		count = count + 1
+	end
+end
+
+-- dump(): For debugging, dump the lists for o.
+function TimeList:dump()
+	print("====DUMP: ", o)
+	local f = self.firstFrame
+	local c = 1
+	while f ~= nil do
+		print("\t"..c..":\t")
+		for j,w in pairs(f) do
+			print ("\t", j, w)
+		end
+		f = f.nextFrame
+		c = c+1
+	end
+	print("====/DUMP")	
+end
+
+
+function TimeList:tableToSave()
+	return {firstFrame=self.firstFrame}
+end
+
+
+function TimeList:loadFromTable(table)
+	self.firstFrame=table.firstFrame
+end
+
+
+return basemodel.timelist
