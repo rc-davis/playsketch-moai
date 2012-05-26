d568327397799f0d83b0d6a3b1a66fa7ca8fb2e5
Ryder Ziola <ryder@ryderziola.ca>
2012-05-26 18:31:56 +0800

    Util helper for deleting by reference

--- src/common-lua/util/util.lua
+++ src/common-lua/util/util.lua
@@ -26,4 +26,14 @@ util = {}
 require "util/debugging"
 require "util/pickle"
 
+
+function util.tableDelete(tab, valref)
+	for i=1,#tab do
+		if tab[i] == valref then
+			table.remove(tab, i)
+			return
+		end
+	end
+end
+
 return util
