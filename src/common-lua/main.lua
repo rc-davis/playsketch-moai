e84ca052dfa6ef2aad7832c6af9185cb54dcc2fc
Ryder Ziola <ryder@ryderziola.ca>
2012-05-28 13:57:21 +0800

    Move current interface out of main into interfaces directory

--- src/common-lua/main.lua
+++ src/common-lua/main.lua
@@ -14,24 +14,22 @@
 	main.lua
 
 	The main entry point for the app.
-	Creates the UI, wires up to controllers, and starts the run loop.
+	Loads the interface from a file in the interfaces/ directory.
 
 --]]
-
 
 require "widgets/widgets"
 require "controllers/controllers"
 require "input/input"
 require "test/test"
 
--- set up window
+-- set up a window
 WIDTH = MOAIEnvironment.screenWidth or 800
 HEIGHT = MOAIEnvironment.screenHeight or 650
 SCALED_WIDTH = WIDTH
 SCALED_HEIGHT = HEIGHT
 if MOAIEnvironment.iosRetinaDisplay then
-	SCALED_WIDTH = WIDTH/2
-	SCALED_HEIGHT = HEIGHT/2
+	SCALED_WIDTH,SCALED_HEIGHT  = WIDTH/2, HEIGHT/2
 end
 
 MOAISim.openWindow ( "playsketch2", WIDTH, HEIGHT )
@@ -49,97 +47,9 @@ MOAISim.pushRenderPass ( drawingLayer )
 widgets.init(viewport) -- this needs to go after our drawing layer is made
 controllers.recording.initManipulator()
 
---TEMP: test function for adding an image
-local function addImage(path)
-	print ("requested to add image at ", path)
-	local userGfx = MOAIGfxQuad2D.new ()
-	userGfx:setTexture (path)
-	userGfx:setRect ( -130, -100, 130, 130 )
-	local userprop = MOAIProp2D.new ()
-	userprop:setDeck ( userGfx )
-	userprop:setLoc(0,0)
-	userprop.isSelected = false
-	userprop.points = {-130, -130, 130, -130, 130, 130, -130, 130 }
-	model.addProp(userprop)
-	
-end
 
-
---TEMP: Build the UI buttons (this is temporary to test the functionality as we code it)
-
-
---Button for generating random lines
-widgets.newSimpleButton(-SCALED_WIDTH/2+125,-SCALED_HEIGHT/2+125,50,50, 
-						"resources/button_generate_lines.png", "resources/button_down.png", nil, 
-						function(_) test.helpers.generateLines(50,50) end, nil )
-
---Timeline buttons
-local slider = widgets.newSlider(25, -SCALED_HEIGHT/2+25, SCALED_WIDTH-50, 50,
-							"resources/slider_background.png",
-							"resources/slider_button.png", 
-							"resources/slider_button_down.png", 
-							 controllers.timeline.sliderMoved,
-							 controllers.timeline.sliderMoveFinished)
-
-
-local playButton = widgets.newToggleButton(-SCALED_WIDTH/2+25,-SCALED_HEIGHT/2+25, 50, 50, 
-							{"resources/button_play.png", "resources/button_pause.png"},
-							"resources/button_down.png", 
-							"resources/button_play_disabled.png",
-							controllers.timeline.playPause)
-controllers.timeline.setButtons(slider, playButton)
-
-
-
-g_keyframeWidget= widgets.keyframes.new(25, -SCALED_HEIGHT/2+75, SCALED_WIDTH-50-50, 50)
-g_recButton = widgets.newSimpleButton(-SCALED_WIDTH/2+25, -SCALED_HEIGHT/2+75, 50, 50,
-					 "resources/button_record.png",
-					"resources/button_stop.png",
-					"resources/button_record_disabled.png", 
-					controllers.recording.recordingButtonUp, 
-					controllers.recording.recordingButtonDown)
-g_recButton:setEnabled(false)
-
---picking draw mode vs selection mode
-widgets.newToggleButton(-SCALED_WIDTH/2+175,-SCALED_HEIGHT/2+125, 50, 50, 
-						{"resources/button_draw.png", "resources/button_select.png"}, 
-						"resources/button_down.png",
-						nil,
-						input.strokecapture.changeMode)
-
-widgets.newSimpleButton(-SCALED_WIDTH/2+225,-SCALED_HEIGHT/2+125,50,50, 
-						"resources/button_save.png", "resources/button_down.png", nil, 
-						controllers.disk.saveToDisk, nil)
-
-widgets.newSimpleButton(-SCALED_WIDTH/2+275,-SCALED_HEIGHT/2+125,50,50, 
-						"resources/button_load.png", "resources/button_down.png", nil, 
-						controllers.disk.loadFromDisk, nil)
-
-
-
-widgets.newSimpleButton(-SCALED_WIDTH/2+325,-SCALED_HEIGHT/2+125,50,50, 
-						"resources/button_clear.png", "resources/button_down.png", nil, 
-						model.deleteAll, nil)
-
---Photo library buttons
-if MOAIPhotoPickerIOS then
-	local b1x,b1y = -SCALED_WIDTH/2+25,  -SCALED_HEIGHT/2+125
-	local b2x,b2y = -SCALED_WIDTH/2+75, -SCALED_HEIGHT/2+125
-	widgets.newSimpleButton(b1x,b1y,50,50, 
-							"resources/button_photolibrary.png", "resources/button_down.png", nil, 
-							function(_) 
-								local wx,wy=drawingLayer:worldToWnd(b1x,b1y) 
-								MOAIPhotoPickerIOS:showPhotoPicker(MOAIPhotoPickerIOS.PhotoPicker_LIBRARY, wx,wy, addImage)
-							end,
-							nil)
-	
-	widgets.newSimpleButton(b2x,b2y,50,50, 
-							"resources/button_photocamera.png", "resources/button_down.png", nil, 
-							function(_) 
-								local wx,wy=drawingLayer:worldToWnd(b2x,b2y) 
-								MOAIPhotoPickerIOS:showPhotoPicker(MOAIPhotoPickerIOS.PhotoPicker_CAMERA, wx,wy, addImage)
-							end, nil)
-end
+--LOAD AN INTERFACE HERE!
+require "interfaces/old-test-interface" 
 
 
 -------- MAIN LOOP
